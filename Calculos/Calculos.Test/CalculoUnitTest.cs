1fe2a975c49b0a5a29bd7af5446a09185784f9e0
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-08-01 09:50:41 -0300

    Add project files.

---
+++ Calculos/Calculos.Test/CalculoUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos.Test
+{
+    public class CalculoUnitTest
+    {
+        // AAA: Arrange, Act, Assert (agir, organizar e provar)
+
+        [Fact]
+        public void TestarMetodoSomar() 
+        {
+            // Arrange: organizar
+            var x1 = 4.1;
+            var x2 = 5.9;
+            var valorEsperado = 10;
+
+            // Act: agir
+            var soma = Calculo.Somar(x1, x2);
+
+            // Assert: provar
+            Assert.Equal(valorEsperado, soma);
+        }
+    }
+}
