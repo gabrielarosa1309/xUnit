3b8d4d136c3450a46b23a0406f636d16dc00850a
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-08-02 08:43:03 -0300

    :construction: Exercício 2 feito

---
+++ Validação de Email/Email/Email.Test/EmailUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email.Test
+{
+    public class ValidacaoUnitTest
+    {
+        [Theory]
+        [InlineData("user@email.com", true)]
+        [InlineData("user.email.com", false)]
+        [InlineData("user@email", false)]
+        [InlineData("user@.com", false)]
+        [InlineData("user@email.com.br", true)]
+        [InlineData("user@email..com", true)]  
+        [InlineData("user@email.com.", true)]  
+        public void TestarMetodoValidarEmail(string email, bool resultadoEsperado)
+        {
+            // Act: agir
+            var resultado = Validacao.ValidarEmail(email);
+
+            // Assert: provar
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+    }
+}
---
+++ Validação de Email/Email/Email/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email
+{
+    public static class Validacao
+    {
+        public static bool ValidarEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            int arroba = email.IndexOf('@');
+            int ponto = email.LastIndexOf('.');
+
+            return arroba > 0 && ponto > arroba + 1 && ponto < email.Length - 1;
+        }
+    }
+}
