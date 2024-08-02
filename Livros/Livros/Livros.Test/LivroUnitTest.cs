77b41b587498aa696e0c0d61d5067eb1dfd076e1
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-08-02 08:25:46 -0300

    :construction: Exercício 1 feito

---
+++ Livros/Livros/Livros.Test/LivroUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros.Test
+{
+    public class BibliotecaUnitTest
+    {
+        [Fact]
+        public void TestarMetodoAdicionarLivro()
+        {
+            // Arrange: organizar
+            var biblioteca = new Biblioteca();
+            var livro = new Livro("Jantar Secreto", "Raphael Montes");
+
+            // Act: agir
+            biblioteca.AdicionarLivro(livro);
+            var livros = biblioteca.GetLivros();
+
+            // Assert: provar
+            Assert.Contains(livro, livros);
+        }
+    }
+}
---
+++ Livros/Livros/Livros/Biblioteca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+
+        public Livro(string titulo, string autor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+        }
+    }
+
+    public class Biblioteca
+    {
+        private List<Livro> livros;
+
+        public Biblioteca()
+        {
+            livros = new List<Livro>();
+        }
+
+        public void AdicionarLivro(Livro livro)
+        {
+            livros.Add(livro);
+        }
+
+        public List<Livro> GetLivros()
+        {
+            return livros;
+        }
+    }
+}
