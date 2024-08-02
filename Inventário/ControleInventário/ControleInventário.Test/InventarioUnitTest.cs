cbe03a7885600fe13de40545fef409324dc3d645
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-08-02 09:23:59 -0300

    :construction: Exercício 4 feito

---
+++ Inventário/ControleInventário/ControleInventário.Test/InventarioUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleInventário.Test
+{
+    public class InventarioUnitTest
+    {
+        //[Fact]
+        //public void TestarMetodoAdicionarProdutoNovo()
+        //{
+        //    // Arrange: organizar
+        //    var inventario = new Inventario();
+        //    var nomeProduto = "Caneta";
+        //    var quantidadeProduto = 10;
+
+        //    // Act: agir
+        //    inventario.AdicionarProduto(nomeProduto, quantidadeProduto);
+        //    var quantidadeAtual = inventario.GetQuantidade(nomeProduto);
+
+        //    // Assert: provar
+        //    Assert.Equal(quantidadeProduto, quantidadeAtual);
+        //}
+
+        //[Fact]
+        //public void TestarMetodoIncrementarQuantidadeProdutoExistente()
+        //{
+        //    // Arrange: organizar
+        //    var inventario = new Inventario();
+        //    var nomeProduto = "Caneta";
+        //    var quantidadeInicial = 10;
+        //    var quantidadeAdicional = 5;
+        //    inventario.AdicionarProduto(nomeProduto, quantidadeInicial);
+
+        //    // Act: agir
+        //    inventario.AdicionarProduto(nomeProduto, quantidadeAdicional);
+        //    var quantidadeAtual = inventario.GetQuantidade(nomeProduto);
+
+        //    // Assert: provar
+        //    Assert.Equal(quantidadeInicial + quantidadeAdicional, quantidadeAtual);
+        //}
+
+        [Fact]
+        public void TestarMetodoObterQuantidadeProdutoNaoExistente()
+        {
+            // Arrange: organizar
+            var inventario = new Inventario();
+            var nomeProduto = "Lápis";
+
+            // Act: agir
+            var quantidadeAtual = inventario.GetQuantidade(nomeProduto);
+
+            // Assert: provar
+            Assert.Equal(0, quantidadeAtual);
+        }
+    }
+}
---
+++ Inventário/ControleInventário/ControleInventário/Inventario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleInventário
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, int quantidade)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+        }
+    }
+
+    public class Inventario
+    {
+        private Dictionary<string, Produto> produtos;
+
+        public Inventario()
+        {
+            produtos = new Dictionary<string, Produto>();
+        }
+
+        public void AdicionarProduto(string nome, int quantidade)
+        {
+            if (produtos.ContainsKey(nome))
+            {
+                produtos[nome].Quantidade += quantidade;
+            }
+            else
+            {
+                produtos[nome] = new Produto(nome, quantidade);
+            }
+        }
+
+        public int GetQuantidade(string nome)
+        {
+            if (produtos.ContainsKey(nome))
+            {
+                return produtos[nome].Quantidade;
+            }
+            return 0;
+        }
+    }
+}
